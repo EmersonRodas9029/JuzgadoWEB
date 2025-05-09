2da78821ea81eaee1d3c6710890d48a79f13fa05
ORSTED9029 <theripper446@gmail.com>
2025-05-09 14:17:03 -0600

    Creacion de página base de login y configuración de acceso con Thymeleaf

---
+++ src/main/java/com/juzgado/gestor_expedientes/GestorExpedientesApplication.java
@@ -0,0 +1,13 @@
+package com.juzgado.gestor_expedientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorExpedientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorExpedientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juzgado/gestor_expedientes/GestorExpedientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juzgado.gestor_expedientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorExpedientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
